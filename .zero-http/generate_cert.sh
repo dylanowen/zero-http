89389e68823e90b51f8fd67074c28a3ffcc87a87
Dylan Owen <dyltotheo@gmail.com>
2018-03-28 16:59:57 -0700

    made some changes to the localhost cert

--- .zero-http/generate_cert.sh
+++ .zero-http/generate_cert.sh
@@ -14,17 +14,17 @@ CA_CERT="zero-http_ca.pem"
 LOCAL_KEY="zero-http.key"
 LOCAL_SIGNED_CERT="zero-http.pem"
 
-CHAIN_FILE="chain.pem"
-KEYSTORE="keystore.p12"
-
 ALIAS="zero-http"
 
 # Generate a new, self-signed root CA
 openssl req -extensions v3_ca -new -x509 -days $DAYS -nodes -subj "/CN=${ALIAS}" -newkey rsa:2048 -sha512 -out $CA_CERT -keyout $CA_KEY
 
-LOCAL_KEY_TEMP="zero-http.temp.key"
+LOCAL_KEY_TEMP="temp.key"
 # Generate a certificate/key for the server to use for Hostname Verification via localhost
 openssl req -new -keyout $LOCAL_KEY_TEMP -nodes -newkey rsa:2048 -subj $SUBJECT | \
     openssl x509 -req -CAkey $CA_KEY -CA $CA_CERT -days $DAYS -set_serial $RANDOM -sha512 -extfile v3.ext -out $LOCAL_SIGNED_CERT
 openssl pkcs8 -topk8 -inform PEM -outform PEM -in $LOCAL_KEY_TEMP -out $LOCAL_KEY -nocrypt
 rm $LOCAL_KEY_TEMP
+
+# remove the CA key since we should never need it again
+rm $CA_KEY
